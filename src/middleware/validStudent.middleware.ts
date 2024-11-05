b6a0bf17822b36710644b24565febb13d387ccae
Pawel Olejniczak <pawel.olejniczak.i@gmail.com>
2024-11-05 21:41:54 +0000

    some New changes (#1)
    * change 2
    
    * new change 1
    
    * update changes

--- src/middleware/validStudent.middleware.ts
+++ src/middleware/validStudent.middleware.ts
@@ -8,12 +8,13 @@ export class ValidStudentMiddleware implements NestMiddleware {
 
   async use(req: Request, res: Response, next: NextFunction) {
     const { studentId } = req.params;
-
+    // add extra change comment
     const student = await this.studentRepository.findByPk(parseInt(studentId));
     if (!student) {
       throw new NotFoundException('Student not found');
     }
-
+    // change 2 to student.id
+    // change 1
     next();
   }
 }
